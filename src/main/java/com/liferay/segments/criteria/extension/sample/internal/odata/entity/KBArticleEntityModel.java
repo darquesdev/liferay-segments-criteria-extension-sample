b1e9a53f35b9c2599b980855993cebfbb94ec644
David Arques <david.arques@liferay.com>
2020-09-16 13:52:08 +0200

    Step 1: Create the Entity Model for KBArticle
    This class represents the associated entity (in this case, the KBArticle) with its fields of interest. These fields are searchable using OData(https://www.odata.org/).

---
+++ src/main/java/com/liferay/segments/criteria/extension/sample/internal/odata/entity/KBArticleEntityModel.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.criteria.extension.sample.internal.odata.entity;
+
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.odata.entity.EntityField;
+import com.liferay.portal.odata.entity.EntityModel;
+import com.liferay.portal.odata.entity.IdEntityField;
+import com.liferay.portal.odata.entity.StringEntityField;
+
+import java.util.Map;
+
+/**
+ * Provides the entity data model from the KB Article.
+ *
+ * @author Eduardo García
+ * @author David Arques
+ */
+public class KBArticleEntityModel implements EntityModel {
+
+	public static final String NAME = "KBArticle";
+
+	public KBArticleEntityModel() {
+		_entityFieldsMap = EntityModel.toEntityFieldsMap(
+			new StringEntityField("title", locale -> "titleKeyword"),
+			new IdEntityField(
+				"kbArticleId", locale -> Field.ENTRY_CLASS_PK,
+				String::valueOf));
+	}
+
+	@Override
+	public Map<String, EntityField> getEntityFieldsMap() {
+		return _entityFieldsMap;
+	}
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	private final Map<String, EntityField> _entityFieldsMap;
+
+}
