b00ee9973fbf3c7f6b186df2c12b36805b20ac31
David Arques <david.arques@liferay.com>
2020-09-19 16:22:39 +0200

    Step 4: Make Item Selector response compatible with Segments Editor UI

---
+++ src/main/java/com/liferay/segments/criteria/extension/sample/internal/servlet/taglib/KBArticleSegmentsCriteriaTopHeadJSPDynamicInclude.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.criteria.extension.sample.internal.servlet.taglib;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.taglib.BaseJSPDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author David Arques
+ */
+@Component(immediate = true, service = DynamicInclude.class)
+public class KBArticleSegmentsCriteriaTopHeadJSPDynamicInclude
+	extends BaseJSPDynamicInclude {
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register(
+			"/html/common/themes/top_head.jsp#post");
+	}
+
+	@Override
+	protected String getJspPath() {
+		return "/dynamic_include/top_head.jsp";
+	}
+
+	@Override
+	protected Log getLog() {
+		return _log;
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.segments.criteria.extension.sample)",
+		unbind = "-"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		KBArticleSegmentsCriteriaTopHeadJSPDynamicInclude.class);
+
+}
